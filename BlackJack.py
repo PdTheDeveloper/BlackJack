efd30c3aaea3aedeb031bfbab2649a1f5a2f6be0
PdTheDeveloper <www.p.dadsetan@gmail.com>
2020-11-03 14:37:29 +0330

    Added ace move and some main functionality

--- BlackJack.py
+++ BlackJack.py
@@ -29,6 +29,7 @@ class Deck:
         self.count=len(cards)
         self.cards=cards
         self.sum=0
+        self.acesDone=0
         for card in self.cards:
             self.sum+=card.value()
 
@@ -46,7 +47,20 @@ class Deck:
 
     def show(self):
         for x in range(len(self.cards)):
-            print(f"{x}. {self.cards[x]}\n")
+            print(f"{x+1}. {self.cards[x]}\n")
+
+    def aceMove(self):
+        while self.aces()>self.acesDone and self.sum>21:
+            self.sum-=10
+            self.acesDone+=1
+
+    def aces(self):
+        a=0
+        for card in self.cards:
+            if card.value()==11:
+                a+=1
+        return a
+
 
     def sum(self):
         return self.sum
@@ -58,23 +72,25 @@ class Deck:
         self.cards[position-1].hide()
 
 
-playerMoney=0
 
 def addMoney(amount):
     global playerMoney
     playerMoney+=amount
 
-def busted(deck):
-    if deck.sum()>21:
-        return True
+def bust(deck):
+    global gamblerIsDone
+    if gamblerIsDone:
+        print("Dealer Busted!")
+        win()
     else:
-        return False
+        print("Gambler Busted!")
+        lose()
 
-
-def drawCards():
-    pass
 
 #Globals
+playerMoney=0
+bet=0
+
 nums={"Ace":11,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,"Jack":10,"Queen":10,"King":10}
 types=["Heart","Diamond","Club","Spade"]
 
@@ -86,18 +102,55 @@ for i in nums.items():
 
 dealerDeck=None
 gamblerDeck=None
+gamblerIsDone=False
+
+def stay():
+    global gamblerIsDone
+    gamblerIsDone=True
+
+def hit():
+    global gamblerDeck,dealerDeck,gamblerIsDone
+    if not gamblerIsDone:
+        fullDeck.draw(gamblerDeck)
+        if gamblerDeck.sum()>21:
+            gamblerDeck.aceMove()
+        if gamblerDeck.sum()>21:
+            bust(gamblerDeck)
+        else:
+            updateTable(dealerDeck,gamblerDeck)
+    else:
+        fullDeck.draw(dealerDeck)
+        if dealerDeck.sum() > 21:
+            dealerDeck.aceMove()
+        if dealerDeck.sum() > 21:
+            bust(dealerDeck)
+        else:
+            updateTable(dealerDeck, gamblerDeck)
 
 
-def setCards():
+
+
+def resetCards():
     global fullCards,fullDeck
     random.shuffle(fullCards)
     fullDeck=None
     fullDeck=Deck(fullCards)
 
 def init():
-    global dealerDeck,gamblerDeck,fullDeck
+    global dealerDeck,gamblerDeck,fullDeck,playerMoney,bet
     print(35*"\n")
-    setCards()
+    while True:
+        try:
+            bet=int(input("How much do you wanna gamble?\n"))
+        except:
+            print("\nInvalid bet! Try again :\n")
+            continue
+        if bet>playerMoney:
+            playerMoney-=bet
+            break
+        else:
+            print("\nNot enough money! Try again :\n")
+    resetCards()
     dealerDeck = None
     gamblerDeck = None
     for x in range(2):
@@ -107,5 +160,27 @@ def init():
 
 
 
-def updateTable(dealerDeck,gamblerDeck):
+def updateTable(dealerDeckP,gamblerDeckP):
+    global gamblerDeck,dealerDeck,gamblerIsDone
     print(35*"\n")
+    print("You :\n\n")
+    gamblerDeck.show()
+    print("\n-----------------------------------------------------\n\nDealer:\n\n")
+    dealerDeck.show()
+    if not gamblerIsDone:
+        while True:
+            try:
+                choice=int(input("\nDo you wanna hit or stay? 1 for hit and 2 for stay"))
+            except:
+                print("\nInvalid choice! Try again:")
+                continue
+            if choice==1 or choice==2:
+                if choice==1:
+                    hit()
+                else:
+                    stay()
+                break
+            else:
+                print("\nInvalid choice! Try again:")
+                continue
+
